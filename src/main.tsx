747d19a5354767db756bffd27ea89bd02351e482
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-15 16:24:18 +0800

    refactor: added routes and authentication in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import { HomePage } from "./pages/home/page.tsx";
+import { AuthenticationProvider } from "./context/Authentication.tsx";
+import LoginPage from "./pages/auth/Login.tsx";
+import Registration from "./pages/auth/Registration.tsx";
+import { Navigation } from "./components/Navigation.tsx";
+import ProtectedRoute from "./pages/common/ProtectedRoute.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthenticationProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route element={<Navigation />}>
+              <Route index element={<HomePage />} />
+              {/* <Route path="nested_route" element={<HomePage />} /> */}
+            </Route>
+          </Route>
+        </Routes>
+      </AuthenticationProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
