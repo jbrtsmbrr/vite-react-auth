75ad7964051d674ed656ff8c528940303ca4df1d
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-15 16:22:55 +0800

    init: AuthentactionProvider component

---
+++ src/context/Authentication.tsx
@@ -0,0 +1,64 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+export interface AuthenicatedUser {
+  id: string;
+  name: string;
+  lastname: string;
+  age: number;
+  username: string;
+}
+
+const AuthenticationContext = createContext<{
+  user: AuthenicatedUser | null;
+  setUser: React.Dispatch<React.SetStateAction<AuthenicatedUser | null>>;
+  loading: boolean;
+} | null>(null);
+
+export const useAuthentication = () => {
+  const context = useContext(AuthenticationContext);
+
+  if (!context)
+    throw new Error(
+      "useAuthentication must be used within a AuthenticationProvider"
+    );
+
+  return context;
+};
+
+export const AuthenticationProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useState<AuthenicatedUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async function () {
+      setLoading(true);
+      const response = await fetch("http://localhost:4004/auth/current", {
+        credentials: "include",
+      });
+      const result = (await response.json()) as {
+        data: {
+          user: AuthenicatedUser | null;
+        } | null;
+        errors: { message: string }[];
+        success: boolean;
+      };
+
+      setUser(result?.data?.user ?? null);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <AuthenticationContext.Provider
+      value={{ user, setUser, loading }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
