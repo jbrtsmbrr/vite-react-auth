e18cdf4516b11e7a0a0db06438747b3fddcd91a4
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-21 23:06:16 +0800

    refactor: updated ui of Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+      className="disabled:bg-gray-300/70 disabled:text-gray-500 disabled:placeholder:text-gray-400 disabled:outline-0 block w-full rounded-md bg-gray-200/70 px-3 py-1.5 text-base text-gray-900 outline-gray-300 placeholder:text-gray-500 placeholder:tracking-wide placeholder:text-xs focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-xs/6"
     />
   );
 }
