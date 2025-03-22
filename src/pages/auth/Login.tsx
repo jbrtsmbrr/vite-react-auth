ee5ef9b937a40e866dbfe3cd55f194775b1a8c52
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-22 23:08:59 +0800

    refactor: update ui of LoginPage

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Input } from "../../components/Input";
-import { Navigate, useNavigate } from "react-router";
+import { Link, Navigate, useNavigate } from "react-router";
 import { useAuthentication } from "../../context/Authentication";
+import Logo from "../../components/Logo";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -13,7 +14,7 @@ export default function LoginPage() {
   const [success, setSuccess] = useState(false);
 
   //   Router functions
-//   const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
   const { user, loading: authenticating } = useAuthentication();
 
@@ -55,24 +56,12 @@ export default function LoginPage() {
       success: boolean;
     };
 
-    // TODO: Handle Refresh token
-    // 1. Setup Routing
-    // 2. Add Authentication Context
-    // 3. Create Custom fetch function to handle refresh token
-
-    // const response2 = await fetch("http://localhost:4004/auth/current", {
-    //     credentials: 'include'
-    // });
-
-    // const result2 = await response2.json();
-    // console.log(result2)
-    
     if (result.success) {
       setSuccess(true);
       setTimeout(() => {
         setSuccess(false);
-        window.location.href = "/"
-      }, 1000);
+        window.location.href = "/";
+      }, 500);
     }
 
     // Update form
@@ -82,30 +71,32 @@ export default function LoginPage() {
   }
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
-            className="mx-auto h-12 w-auto"
-          />
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="relative flex h-screen min-h-screen justify-evenly items-center">
+      {/* Form */}
+      <div className="relative flex min-h-full flex-1 flex-col justify-center px-6 py-4 lg:px-8">
+        <Link to="/" className="w-fit mx-auto py-4">
+          <Logo />
+        </Link>
+        <div className="flex flex-col items-center justify-center flex-1">
+          <div className="p-8 flex flex-col gap-2">
+            <p className="font-serif text-center text-4xl font-normal tracking-tight text-gray-900">
+              Welcome Back
+            </p>
+            <p className="text-center text-sm text-gray-900">
+              Enter your credentials to access your account
+            </p>
+          </div>
           <form
             action="#"
             method="POST"
-            className="space-y-2"
+            className="space-y-2 w-1/2"
             onSubmit={handleSubmit}
           >
+            {/* Inputs */}
             <div>
               <label
                 htmlFor="username"
-                className="block text-sm/6 font-medium text-gray-900 tracking-wide"
+                className="block font-semibold text-gray-800 tracking-wide text-xs/6"
               >
                 Username
               </label>
@@ -118,6 +109,7 @@ export default function LoginPage() {
                   autoComplete="username"
                   placeholder="Enter your username"
                   value={username}
+                  disabled={loading}
                   onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
@@ -125,7 +117,7 @@ export default function LoginPage() {
             <div>
               <label
                 htmlFor="password"
-                className="block text-sm/6 font-medium text-gray-900 tracking-wide"
+                className="block font-semibold text-gray-800 tracking-wide text-xs/6"
               >
                 Password
               </label>
@@ -138,16 +130,25 @@ export default function LoginPage() {
                   autoComplete="password"
                   placeholder="Enter your password"
                   value={password}
+                  disabled={loading}
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
             </div>
 
+            {/* Forgot password */}
             <div>
+              <Link to="/forgot_password">
+                <p className="text-right text-xs text-indigo-500 font-medium hover:text-indigo-400 tracking-wide">
+                  Forgot password?
+                </p>
+              </Link>
+            </div>
+            <div className="py-2">
               <button
                 disabled={loading}
                 type="submit"
-                className="disabled:bg-indigo-400 disabled:pointer-events-none flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="disabled:bg-black/70 disabled:pointer-events-none flex w-full justify-center rounded-md cursor-pointer bg-black px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800"
               >
                 {loading ? "Verifying..." : "Login"}
               </button>
@@ -155,7 +156,7 @@ export default function LoginPage() {
           </form>
 
           {!loading && errors.length > 0 && (
-            <div className="p-4 bg-red-100 text-red-500 mt-2 border border-red-300/50 text-sm tracking-wide">
+            <div className="w-1/2 p-2 bg-red-100/60 text-red-500 mt-2 border border-red-200/50 text-xs tracking-wider">
               {errors.map((error) => (
                 <p>{error}</p>
               ))}
@@ -163,12 +164,43 @@ export default function LoginPage() {
           )}
 
           {!loading && success && (
-            <div className="p-4 bg-green-50 text-green-600 mt-2 border border-green-300/50 text-sm tracking-wide">
+            <div className="w-1/2 p-2 bg-green-50 text-green-600 mt-2 border border-green-300/50 text-xs tracking-wider">
               <p>Great! You have successfully logged in</p>
             </div>
           )}
         </div>
+        {/* Form footer */}
+        <div>
+          <p className="mt-10 text-center text-sm text-gray-900">
+            Not a member?{" "}
+            <Link
+              to="/register"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
-    </>
+
+      {/* Side UI */}
+      <div className="bg-gray-500 flex-1 h-full">
+        <div
+          className="banner h-full"
+          style={{
+            background: "url(./src/assets/images/abstract.jpg)",
+            // height: "100vh",
+            // backgroundPosition: `65% 51%`,
+            backgroundRepeat: "no-repeat",
+            position: "relative",
+            backgroundSize: "100% 100%",
+          }}
+        ></div>
+        {/* <img
+          src="./src/assets/images/abstract.jpg"
+          className="h-full relative w-full object-fill"
+        /> */}
+      </div>
+    </div>
   );
 }
