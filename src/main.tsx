a35412210818c8981c45a888baf700e93e103cb6
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-22 23:08:44 +0800

    refactor: added new pages in main file

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,8 @@ import LoginPage from "./pages/auth/Login.tsx";
 import Registration from "./pages/auth/Registration.tsx";
 import { Navigation } from "./components/Navigation.tsx";
 import ProtectedRoute from "./pages/common/ProtectedRoute.tsx";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage.tsx";
+import RecoverAccountPage from "./pages/auth/RecoverAccountPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,6 +18,8 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<Registration />} />
+          <Route path="/forgot_password" element={<ForgotPasswordPage />} />
+          <Route path="/recover_account" element={<RecoverAccountPage />} />
           <Route path="/" element={<ProtectedRoute />}>
             <Route element={<Navigation />}>
               <Route index element={<HomePage />} />
